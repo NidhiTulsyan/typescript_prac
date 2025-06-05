c835919f2057125c581c713ef30002b67e2bce21
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2025-06-06 01:32:50 +0530

    main code in ts file , till any 28:07'

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+//declaring statement
+let age = 20;
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//declaring statement
+let age:number=20;
+
+//creating new configuration file by tsc --init
+
+//now we have learned about some configuration settings on tsconfig file
+//uncommented- "noEmitOnError": true,"outDir": "./dist","removeComments": true,  "rootDir": "./src", 
+
+//typescript extended types:
+//any,unknown,enum,tuple,never
+
+//we can declare variable and then remove its dataype then also compiler will know its of that datatype  but if we dont that it consider it as any which means that any value can come of any datatype
