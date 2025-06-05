cad970a13ecd114b39fc19f9fb96ce49b9c8a4e4
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2025-06-06 01:44:42 +0530

    done till enum

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-//declaring statement
 let age = 20;
+let a = [1, 2, 34, 5];
+console.log(a[2]);
+let b = [1, 'nidhi'];
+var size;
+(function (size) {
+    size[size["small"] = 2] = "small";
+    size[size["medium"] = 3] = "medium";
+    size[size["large"] = 4] = "large";
+})(size || (size = {}));
+;
+let mysize = size.medium;
+console.log(mysize);
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,19 @@ let age:number=20;
 //any,unknown,enum,tuple,never
 
 //we can declare variable and then remove its dataype then also compiler will know its of that datatype  but if we dont that it consider it as any which means that any value can come of any datatype
+
+//arrays and tuple we studied
+//arrays can be as
+let a:number[]=[1,2,34,5];
+console.log(a[2]);
+
+//tuple
+let b:[number,string]=[1,'nidhi'];//mostly used as key value pair
+
+//enum
+enum size {small=2,medium,large};
+let mysize:size=size.medium;
+console.log(mysize);
+
+
+
